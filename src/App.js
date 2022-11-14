f99db12d2d2c8d719ce2713bffd29631f7d1726d
10 <tochka098@gmail.com>
2022-11-14 22:13:35 +0400

    finished task

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import MainContent from "./components/MainContent";
+import Main from "./components/MainContent";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <MainContent />
+      <Main />
       <Footer />
     </div>
   );
